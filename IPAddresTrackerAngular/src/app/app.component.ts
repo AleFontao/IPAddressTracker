448871cdf06512f030c0e2eab81e8b787ad72d2a
alejo fontao <alefonta@hotmail.com.ar>
2022-10-18 15:49:01 -0300

    final

--- IPAddresTrackerAngular/src/app/app.component.ts
+++ IPAddresTrackerAngular/src/app/app.component.ts
@@ -1,10 +1,69 @@
-import { Component } from '@angular/core';
-
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import * as L from 'leaflet';
+import { Ubicacion } from 'src/models/ubicacion';
+import { UbicacionService } from 'src/services/ubicacion.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'IPAddresTrackerAngular';
+export class AppComponent implements OnInit {
+  map: any;
+  constructor(private ubicacionService: UbicacionService) {
+
+  }
+  ubicacion: Ubicacion = {
+    ipAddress: '',
+    isp: '',
+    location: '',
+    timezone: ''
+  };
+  flag: boolean = false;
+  ipFormControl = new FormControl('', [Validators.required, Validators.pattern(/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/)]);
+
+
+  ngOnInit(): void {
+    this.map = L.map('map').setView([-33.461582765942882, -64.18693255105764], 15);
+
+    L.tileLayer('https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png', {
+      attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+    }).addTo(this.map);
+  }
+
+  ngAfterViewInit(): void {
+    this.ubicacionService.getIp().subscribe(result => {
+      this.ipFormControl.setValue(result.ip);
+    });
+  }
+
+  consultarIp() {
+    this.ubicacionService.consultarIp(this.ipFormControl.value).subscribe(
+      result => {
+        this.ubicacion = {
+          ipAddress: result.ip,
+          isp: result.isp,
+          location: result.location.region + ", " + result.location.country,
+          timezone: result.location.timezone,
+        }
+        
+        this.ubicacionService.getAddressFromAddress(result.location.region + "%20," + result.location.country).subscribe(data => {
+          this.map.panTo(new L.LatLng(data.Results[0].latitude, data.Results[0].longitude), { animation: true });
+          L.marker([data.Results[0].latitude, data.Results[0].longitude]).addTo(this.map)
+            .bindPopup('')
+            .openPopup();
+        }
+        );
+        this.flag = true;
+      }
+    );
+  }
+
+  getErrorMessage() {
+    if (this.ipFormControl.hasError('required')) {
+      return 'You must enter a value';
+    }
+    return this.ipFormControl.hasError('pattern') ? 'Not a valid ip' : '';
+  }
 }
+
--- IPAddresTrackerAngular/src/app/app.module.ts
+++ IPAddresTrackerAngular/src/app/app.module.ts
@@ -4,6 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -12,7 +18,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatInputModule,
+    HttpClientModule,
+    MatFormFieldModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
